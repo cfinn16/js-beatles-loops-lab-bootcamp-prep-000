7f00088612c8c68a534b789ab2d7e1fade8e293e
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-18 20:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var arr = {};
+  arr = {};
   
   for (let i = 0; i < 4; i++) {
     arr.unshift(`musicians[i] + " plays " + instruments[i]`);
