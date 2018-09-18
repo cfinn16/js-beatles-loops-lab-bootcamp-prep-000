86dda9a7dd377e1a48b6bbf0355b97c0b15c3b55
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-18 20:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var newArray = {};
   
   for (let i = 0; i < 4; i++) {
-    newArray.pop(musicians[i] + " plays " + instruments[i]);
+    newArray.pop(`musicians[i] + " plays " + instruments[i]`);
   }
   return newArray;
 }
