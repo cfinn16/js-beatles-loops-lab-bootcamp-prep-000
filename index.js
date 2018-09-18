4349e70da0ba8d3ca842a8f4f647eae95f64e9f1
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-18 20:52:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var arr = {};
+  var arr = [];
   
   for (var i = 0; i < 4; i++) {
     arr.splice(0, 0, `musicians[i] + " plays " + instruments[i]`);
